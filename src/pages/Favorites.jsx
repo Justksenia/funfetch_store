430303cb12c2b8efeebf7aa002940196ac36a467
Ksenia Fokina <ksesha163@mil.ru>
2022-08-22 20:12:03 +0400

    fifth

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -5,11 +5,16 @@ const Favorites = ({favoriteItems,  onRemoveFavorite}) => {
   return (
     <div>
       <h2>Мое избранное</h2>
+     
+      
+      
       <div className="list__item">
-        {[...favoriteItems].map((item) => (
+      {favoriteItems.length<1?<h3 >Добавьте что-нибудь в избранное</h3>
+      : 
+        [...favoriteItems].map((item) => (
           <Card {...item} key={item.id} like={true} onRemoveFavorite={()=>onRemoveFavorite(item.id)}/>
         ))}
-      </div>
+      </div> 
     </div>
   );
 };
